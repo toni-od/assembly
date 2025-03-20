1acfadaf90abc9101bee39f24eae2bc0ddbe9352
toni-od <samaelsamael662@gmail.com>
2025-03-20 17:10:50 +0300

    feat: finc the maximum of a list of numbers

---
+++ max.s
@@ -0,0 +1,38 @@
+# PURPOSE:
+#
+# This program finds the maximum number of a set of data items.
+
+# VARIABLES: The registers have the following uses:
+#
+# %edi - Holds the index of the data item being examined
+# %ebx - Largest data item found
+# %eax - Current data item
+#
+# The following memory locations are used:
+#
+# data_items - contains the item data. A 0 is used
+# to terminate the data
+#
+.section .data
+data_items:
+#These are the data items
+.long 1, 3, 9, 4, 3,0
+
+.section .text
+.globl _start
+_start:
+movl $0, %edi 
+movl data_items(,%edi,4), %eax
+movl %eax, %ebx
+start_loop:
+cmpl $0, %eax
+je loop_exit
+incl %edi
+movl data_items(,%edi,4), %eax
+cmpl %ebx, %eax 
+jle start_loop
+movl %eax, %ebx
+jmp start_loop
+loop_exit:
+movl $1, %eax
+int $0x80
